0dae461da19ae795a18b0822a4546ee3ab25a3c8
Chris Barr <cbarr@appriver.com>
2015-07-28 10:55:55 -0400

    Renaming duplicate var declarations

--- bootstrap-notify.js
+++ bootstrap-notify.js
@@ -22,36 +22,36 @@
 }(function ($) {
 	// Create the defaults once
 	var defaults = {
-			element: 'body',
-			position: null,
-			type: "info",
-			allow_dismiss: true,
-			newest_on_top: false,
-			showProgressbar: false,
-			placement: {
-				from: "top",
-				align: "right"
-			},
-			offset: 20,
-			spacing: 10,
-			z_index: 1031,
-			delay: 5000,
-			timer: 1000,
-			url_target: '_blank',
-			mouse_over: null,
-			animate: {
-				enter: 'animated fadeInDown',
-				exit: 'animated fadeOutUp'
-			},
-			onShow: null,
-			onShown: null,
-			onClose: null,
-			onClosed: null,
-			icon_type: 'class',
-			template: '<div data-notify="container" class="col-xs-11 col-sm-4 alert alert-{0}" role="alert"><button type="button" aria-hidden="true" class="close" data-notify="dismiss">&times;</button><span data-notify="icon"></span> <span data-notify="title">{1}</span> <span data-notify="message">{2}</span><div class="progress" data-notify="progressbar"><div class="progress-bar progress-bar-{0}" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%;"></div></div><a href="{3}" target="{4}" data-notify="url"></a></div>'
-		};
+		element: 'body',
+		position: null,
+		type: "info",
+		allow_dismiss: true,
+		newest_on_top: false,
+		showProgressbar: false,
+		placement: {
+			from: "top",
+			align: "right"
+		},
+		offset: 20,
+		spacing: 10,
+		z_index: 1031,
+		delay: 5000,
+		timer: 1000,
+		url_target: '_blank',
+		mouse_over: null,
+		animate: {
+			enter: 'animated fadeInDown',
+			exit: 'animated fadeOutUp'
+		},
+		onShow: null,
+		onShown: null,
+		onClose: null,
+		onClosed: null,
+		icon_type: 'class',
+		template: '<div data-notify="container" class="col-xs-11 col-sm-4 alert alert-{0}" role="alert"><button type="button" aria-hidden="true" class="close" data-notify="dismiss">&times;</button><span data-notify="icon"></span> <span data-notify="title">{1}</span> <span data-notify="message">{2}</span><div class="progress" data-notify="progressbar"><div class="progress-bar progress-bar-{0}" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%;"></div></div><a href="{3}" target="{4}" data-notify="url"></a></div>'
+	};
 
-	String.format = function() {
+	String.format = function () {
 		var str = arguments[0];
 		for (var i = 1; i < arguments.length; i++) {
 			str = str.replace(RegExp("\\{" + (i - 1) + "\\}", "gm"), arguments[i]);
@@ -59,9 +59,9 @@
 		return str;
 	};
 
-	function Notify ( element, content, options ) {
+	function Notify(element, content, options) {
 		// Setup Content of Notify
-		var content = {
+		var contentObj = {
 			content: {
 				message: typeof content == 'object' ? content.message : content,
 				title: content.title ? content.title : '',
@@ -71,7 +71,7 @@
 			}
 		};
 
-		options = $.extend(true, {}, content, options);
+		options = $.extend(true, {}, contentObj, options);
 		this.settings = $.extend(true, {}, defaults, options);
 		this._defaults = defaults;
 		if (this.settings.content.target == "-") {
@@ -83,10 +83,10 @@
 		}
 
 		if (typeof this.settings.offset == 'number') {
-		    this.settings.offset = {
-		    	x: this.settings.offset,
-		    	y: this.settings.offset
-		    };
+			this.settings.offset = {
+				x: this.settings.offset,
+				y: this.settings.offset
+			};
 		}
 
 		this.init();
@@ -109,51 +109,51 @@
 
 			this.notify = {
 				$ele: this.$ele,
-				update: function(command, update) {
+				update: function (command, update) {
 					var commands = {};
 					if (typeof command == "string") {
 						commands[command] = update;
-					}else{
+					} else {
 						commands = command;
 					}
-					for (var command in commands) {
-						switch (command) {
+					for (var cmd in commands) {
+						switch (cmd) {
 							case "type":
 								this.$ele.removeClass('alert-' + self.settings.type);
 								this.$ele.find('[data-notify="progressbar"] > .progress-bar').removeClass('progress-bar-' + self.settings.type);
-								self.settings.type = commands[command];
-								this.$ele.addClass('alert-' + commands[command]).find('[data-notify="progressbar"] > .progress-bar').addClass('progress-bar-' + commands[command]);
+								self.settings.type = commands[cmd];
+								this.$ele.addClass('alert-' + commands[cmd]).find('[data-notify="progressbar"] > .progress-bar').addClass('progress-bar-' + commands[cmd]);
 								break;
 							case "icon":
 								var $icon = this.$ele.find('[data-notify="icon"]');
 								if (self.settings.icon_type.toLowerCase() == 'class') {
-									$icon.removeClass(self.settings.content.icon).addClass(commands[command]);
-								}else{
+									$icon.removeClass(self.settings.content.icon).addClass(commands[cmd]);
+								} else {
 									if (!$icon.is('img')) {
 										$icon.find('img');
 									}
-									$icon.attr('src', commands[command]);
+									$icon.attr('src', commands[cmd]);
 								}
 								break;
 							case "progress":
-								var newDelay = self.settings.delay - (self.settings.delay * (commands[command] / 100));
+								var newDelay = self.settings.delay - (self.settings.delay * (commands[cmd] / 100));
 								this.$ele.data('notify-delay', newDelay);
-								this.$ele.find('[data-notify="progressbar"] > div').attr('aria-valuenow', commands[command]).css('width', commands[command] + '%');
+								this.$ele.find('[data-notify="progressbar"] > div').attr('aria-valuenow', commands[cmd]).css('width', commands[cmd] + '%');
 								break;
 							case "url":
-								this.$ele.find('[data-notify="url"]').attr('href', commands[command]);
+								this.$ele.find('[data-notify="url"]').attr('href', commands[cmd]);
 								break;
 							case "target":
-								this.$ele.find('[data-notify="url"]').attr('target', commands[command]);
+								this.$ele.find('[data-notify="url"]').attr('target', commands[cmd]);
 								break;
 							default:
-								this.$ele.find('[data-notify="' + command +'"]').html(commands[command]);
 						};
+								this.$ele.find('[data-notify="' + cmd + '"]').html(commands[cmd]);
 					}
 					var posX = this.$ele.outerHeight() + parseInt(self.settings.spacing) + parseInt(self.settings.offset.y);
 					self.reposition(posX);
 				},
-				close: function() {
+				close: function () {
 					self.close();
 				}
 			};
@@ -169,18 +169,18 @@
 				this.$ele.find('[data-notify="progressbar"]').remove();
 			}
 		},
-		setIcon: function() {
+		setIcon: function () {
 			if (this.settings.icon_type.toLowerCase() == 'class') {
 				this.$ele.find('[data-notify="icon"]').addClass(this.settings.content.icon);
-			}else{
+			} else {
 				if (this.$ele.find('[data-notify="icon"]').is('img')) {
 					this.$ele.find('[data-notify="icon"]').attr('src', this.settings.content.icon);
-				}else{
-					this.$ele.find('[data-notify="icon"]').append('<img src="'+this.settings.content.icon+'" alt="Notify Icon" />');
+				} else {
+					this.$ele.find('[data-notify="icon"]').append('<img src="' + this.settings.content.icon + '" alt="Notify Icon" />');
 				}
 			}
 		},
-		styleDismiss: function() {
+		styleDismiss: function () {
 			this.$ele.find('[data-notify="dismiss"]').css({
 				position: 'absolute',
 				right: '10px',
@@ -188,7 +188,7 @@
 				zIndex: this.settings.z_index + 2
 			});
 		},
-		styleURL: function() {
+		styleURL: function () {
 			this.$ele.find('[data-notify="url"]').css({
 				backgroundImage: 'url(data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7)',
 				height: '100%',
@@ -199,31 +199,31 @@
 				zIndex: this.settings.z_index + 1
 			});
 		},
-		placement: function() {
+		placement: function () {
 			var self = this,
 				offsetAmt = this.settings.offset.y,
 				css = {
 					display: 'inline-block',
 					margin: '0px auto',
-					position: this.settings.position ?  this.settings.position : (this.settings.element === 'body' ? 'fixed' : 'absolute'),
+					position: this.settings.position ? this.settings.position : (this.settings.element === 'body' ? 'fixed' : 'absolute'),
 					transition: 'all .5s ease-in-out',
 					zIndex: this.settings.z_index
 				},
 				hasAnimation = false,
 				settings = this.settings;
 
-			$('[data-notify-position="' + this.settings.placement.from + '-' + this.settings.placement.align + '"]:not([data-closing="true"])').each(function() {
+			$('[data-notify-position="' + this.settings.placement.from + '-' + this.settings.placement.align + '"]:not([data-closing="true"])').each(function () {
 				return offsetAmt = Math.max(offsetAmt, parseInt($(this).css(settings.placement.from)) +  parseInt($(this).outerHeight()) +  parseInt(settings.spacing));
 			});
 			if (this.settings.newest_on_top == true) {
 				offsetAmt = this.settings.offset.y;
 			}
-			css[this.settings.placement.from] = offsetAmt+'px';
+			css[this.settings.placement.from] = offsetAmt + 'px';
 
 			switch (this.settings.placement.align) {
 				case "left":
 				case "right":
-					css[this.settings.placement.align] = this.settings.offset.x+'px';
+					css[this.settings.placement.align] = this.settings.offset.x + 'px';
 					break;
 				case "center":
 					css.left = 0;
@@ -231,14 +231,14 @@
 					break;
 			}
 			this.$ele.css(css).addClass(this.settings.animate.enter);
-			$.each(Array('webkit-', 'moz-', 'o-', 'ms-', ''), function(index, prefix) {
-				self.$ele[0].style[prefix+'AnimationIterationCount'] = 1;
+			$.each(Array('webkit-', 'moz-', 'o-', 'ms-', ''), function (index, prefix) {
+				self.$ele[0].style[prefix + 'AnimationIterationCount'] = 1;
 			});
 
 			$(this.settings.element).append(this.$ele);
 
 			if (this.settings.newest_on_top == true) {
-				offsetAmt = (parseInt(offsetAmt)+parseInt(this.settings.spacing)) + this.$ele.outerHeight();
+				offsetAmt = (parseInt(offsetAmt) + parseInt(this.settings.spacing)) + this.$ele.outerHeight();
 				this.reposition(offsetAmt);
 			}
 
@@ -254,7 +254,7 @@
 				}
 			});
 
-			setTimeout(function() {
+			setTimeout(function () {
 				if (!hasAnimation) {
 					if ($.isFunction(self.settings.onShown)) {
 						self.settings.onShown.call(this);
@@ -262,10 +262,10 @@
 				}
 			}, 600);
 		},
-		bind: function() {
+		bind: function () {
 			var self = this;
 
-			this.$ele.find('[data-notify="dismiss"]').on('click', function() {
+			this.$ele.find('[data-notify="dismiss"]').on('click', function () {
 				self.close();
 			})
 
@@ -278,7 +278,7 @@
 
 			if (this.settings.delay > 0) {
 				self.$ele.data('notify-delay', self.settings.delay);
-				var timer = setInterval(function() {
+				var timer = setInterval(function () {
 					var delay = parseInt(self.$ele.data('notify-delay')) - self.settings.timer;
 					if ((self.$ele.data('data-hover') === 'false' && self.settings.mouse_over == "pause") || self.settings.mouse_over != "pause") {
 						var percent = ((self.settings.delay - delay) / self.settings.delay) * 100;
@@ -292,7 +292,7 @@
 				}, self.settings.timer);
 			}
 		},
-		close: function() {
+		close: function () {
 			var self = this,
 				$successors = null,
 				posX = parseInt(this.$ele.css(this.settings.placement.from)),
@@ -314,7 +314,7 @@
 				}
 			});
 
-			setTimeout(function() {
+			setTimeout(function () {
 				if (!hasAnimation) {
 					self.$ele.remove();
 					if (self.settings.onClosed) {
@@ -323,33 +323,33 @@
 				}
 			}, 600);
 		},
-		reposition: function(posX) {
+		reposition: function (posX) {
 			var self = this,
 				notifies = '[data-notify-position="' + this.settings.placement.from + '-' + this.settings.placement.align + '"]:not([data-closing="true"])',
 				$elements = this.$ele.nextAll(notifies);
 			if (this.settings.newest_on_top == true) {
 				$elements = this.$ele.prevAll(notifies);
 			}
-			$elements.each(function() {
+			$elements.each(function () {
 				$(this).css(self.settings.placement.from, posX);
-				posX = (parseInt(posX)+parseInt(self.settings.spacing)) + $(this).outerHeight();
+				posX = (parseInt(posX) + parseInt(self.settings.spacing)) + $(this).outerHeight();
 			});
 		}
 	});
 
-	$.notify = function ( content, options ) {
-		var plugin = new Notify( this, content, options );
+	$.notify = function (content, options) {
+		var plugin = new Notify(this, content, options);
 		return plugin.notify;
 	};
-	$.notifyDefaults = function( options ) {
+	$.notifyDefaults = function (options) {
 		defaults = $.extend(true, {}, defaults, options);
 		return defaults;
 	};
-	$.notifyClose = function( command ) {
+	$.notifyClose = function (command) {
 		if (typeof command === "undefined" || command == "all") {
 			$('[data-notify]').find('[data-notify="dismiss"]').trigger('click');
-		}else{
-			$('[data-notify-position="'+command+'"]').find('[data-notify="dismiss"]').trigger('click');
+		} else {
+			$('[data-notify-position="' + command + '"]').find('[data-notify="dismiss"]').trigger('click');
 		}
 	};
 
