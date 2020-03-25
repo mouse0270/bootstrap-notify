5f54e901a2abfbf5f8e8759fb3f0cdcf1f80c722
milan-sahana <milan-sahana@users.noreply.github.com>
2020-03-25 17:02:05 +0530

    Create custom-notify-5.0.js

---
+++ custom-notify-5.0.js
@@ -0,0 +1,80 @@
+/*
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css">	
+*/
+
+// $(document).ready(function () {
+    $.fn.cbn = function (var_message, var_type = 'info', update = 'no') {
+        // alert('a');
+        var var_title = '';
+        var var_icon = '';
+        var val_showProgressbar = false;
+        var val_allow_dismiss = true;
+
+        if (var_type == 'success') {
+            var_title = 'Success!! ';
+            var_icon = 'fa fas fa-check-circle-o';
+
+        } else if (var_type == 'danger') {
+            var_title = 'Error!! ';
+            var_icon = 'fas fa-exclamation-circle';
+
+        } else if (var_type == 'warning') {
+            var_title = 'Warning!! ';
+            var_icon = 'fas fa-exclamation-circle';
+        } else if (var_type == 'loading') {
+            var_type = 'info';
+            var_title = 'Loading!! ';
+            var_icon = 'fas fa-refresh fa-spin fa-fw';
+            val_allow_dismiss= false;
+            val_showProgressbar = true;
+        } else if (var_type == 'wait') {
+            var_type = 'info';
+            var_title = 'Wait!! ';
+            var_icon = 'fas fa-refresh fa-spin fa-fw';
+            val_allow_dismiss= false;
+            /* val_showProgressbar = true; */
+        } else {
+            var_type = 'info';
+            var_title = 'Message!! ';
+            var_icon = 'fas fa-info-circle';
+        }
+
+        if(update=='no') {
+            window.notify = $.notify({
+                    // obj.message
+                    // options
+                    icon: var_icon,
+                    title: var_title,
+                    message: var_message,
+                    /*
+                    url: 'https://github.com/mouse0270/bootstrap-notify',
+                    target: '_blank'
+                    */
+                },
+                {
+                    /* allow_dismiss: false, */
+                    type: var_type,
+                    z_index: 6001,
+                    showProgressbar: val_showProgressbar,
+                    allow_dismiss: val_allow_dismiss,
+                    /*
+                    placement: {
+                        from: 'top',
+                        align: 'right'
+                    },
+                    delay: 3000,
+                    timer: 2000,
+                    animate: {
+                        enter: 'animated fadeInDown',
+                        exit: 'animated fadeOutUp'
+                    },
+                    */                
+                });
+        }
+        else {
+            window.notify.update({type: var_type, icon: var_icon, title: var_title, message: var_message,});
+        }
+    };
+
+// });
+
