dbe308fa5fcb655c2eabfaf9d9c45488ad4a757b
milan-sahana <milan-sahana@users.noreply.github.com>
2020-05-03 06:50:01 +0530

    Update custom-notify.js
    delay: var_delay,
    
    update -- bug fixed

--- custom-notify.js
+++ custom-notify.js
@@ -3,7 +3,7 @@
 */
 
 // $(document).ready(function () {
-    $.fn.cbn = function (var_message, var_type = 'info', update = 'no') {
+    $.fn.cbn = function (var_message, var_type = 'info', var_update = 'no', var_delay=5000) {
         // alert('a');
         var var_title = '';
         var var_icon = '';
@@ -26,21 +26,23 @@
             var_title = 'Loading!! ';
             var_icon = 'fa fa-refresh fa-spin fa-fw';
             val_allow_dismiss= false;
-            val_showProgressbar = true;
+            /* val_showProgressbar = true; */
+            var_delay = 0;
         } else if (var_type == 'wait') {
             var_type = 'info';
             var_title = 'Wait!! ';
             var_icon = 'fa fa-refresh fa-spin fa-fw';
-            val_allow_dismiss= false;
+            /* val_allow_dismiss= false; */
             /* val_showProgressbar = true; */
+            var_delay = 0;
         } else {
             var_type = 'info';
             var_title = 'Message!! ';
             var_icon = 'fa fa-info-circle';
         }
 
-        if(update=='no') {
-            window.notify = $.notify({
+        if(var_update=='no') {
+            return window.notify = $.notify({
                     // obj.message
                     // options
                     icon: var_icon,
@@ -57,12 +59,12 @@
                     z_index: 6001,
                     showProgressbar: val_showProgressbar,
                     allow_dismiss: val_allow_dismiss,
+                    delay: var_delay,
                     /*
                     placement: {
                         from: 'top',
                         align: 'right'
                     },
-                    delay: 3000,
                     timer: 2000,
                     animate: {
                         enter: 'animated fadeInDown',
@@ -72,7 +74,14 @@
                 });
         }
         else {
-            window.notify.update({type: var_type, icon: var_icon, title: var_title, message: var_message,});
+            if(typeof(var_update) != 'object') {
+                var_update = window.notify;
+            }
+            
+            var_update.update({type: var_type, icon: var_icon, title: var_title, message: var_message,});
+            setTimeout(function() {
+                var_update.close();
+            }, var_delay);
         }
     };
 
